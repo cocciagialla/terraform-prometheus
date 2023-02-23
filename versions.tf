7bc2b42df22577d4b21a539be4b20859f0669694
Gianluigi 'cocciagialla' Mammarella <cocciagialla@gmail.com>
2023-02-23 19:21:17 +0100

    first commit

--- versions.tf
+++ versions.tf
@@ -2,6 +2,14 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
+    linode = {
+      source = "linode/linode"
+      version = "1.29.4"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.72"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.4.1"
